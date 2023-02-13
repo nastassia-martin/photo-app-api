e72b2beb8e30b2f725abd8fbac8e4b12c72a9102
nastassia martin <nastassia.martin@googlemail.com>
2023-02-13 21:38:04 +0100

    added jwt authentication and a todo list of how to implement jwt

--- src/controllers/profile_controller.ts
+++ src/controllers/profile_controller.ts
@@ -5,16 +5,23 @@ import Debug from 'debug'
 import { Request, Response } from 'express'
 import { validationResult } from 'express-validator'
 import prisma from '../prisma'
+import { getUserbyEmail } from '../services/user_service'
 
 // Create a new debug instance
 const debug = Debug('photo-api:profile_controller 📝')
 
 /**
  * Get authenticated user's profile
+ * 
+ * @todo set up a route in index.ts to /get user profile ✅
+ * @todo set up req.token globally
+ * @todo change tsconfg to reflect global scope
  */
 export const getProfile = async (req: Request, res: Response) => {
+    // send if user has successfully authenticated
+
     res.send({
         status: "success",
-        data: null
+        data: req.user
     })
 }
